6b51445a62d8c5b5e15b7258caba38848958de1a
Randolph Xia <xia.randolph.227@gmail.com>
2018-06-13 15:29:26 -0400

    Set up shop list

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,53 @@
+Vue.component('shop', {
+  template: '#shop-template',
+  props: {
+    data: Array,
+    keys: Array,
+  },
+  data: function() {
+    var sortOrders = {}
+    this.keys.forEach(function(key) {
+      sortOrders[key] = 1 //stores sort order for each column, 1 or -1 for ascending or descending
+    })
+    return {
+      sortKey: '',
+      sortOrders: sortOrders
+    }
+  },
+  computed: {
+    filteredData: function() {
+      var sortKey = this.sortKey
+      var order = this.sortOrders[sortKey] || 1
+      var data = this.data
+
+      if (sortKey) { //sorts data based on column sortKey and order
+        data = data.slice().sort(function(a, b) {
+          a = a[sortKey]
+          b = b[sortKey]
+          return (a === b ? 0 : a > b ? 1 : -1) * order
+        })
+      }
+      return data
+    }
+  },
+  filters: {
+    capitalize: function(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+  },
+  methods: {
+    sortBy: function(key) {
+      this.sortKey = key
+      this.sortOrders[key] = this.sortOrders[key] * -1
+    }
+  }
+})
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Still empty, but using Vue now!'
+    motd: "Welcome to the shop",
+    shopKeys: ['name', 'description', 'price'],
+    shopData: productList //defined in products.js
   }
 })
---
+++ src/js/products.js
@@ -0,0 +1,13 @@
+var productList = [{
+  name: 'Product 1',
+  description: 'The first of many',
+  price: 5
+}, {
+  name: 'Product 2',
+  description: "The second one",
+  price: 7
+}, {
+  name: 'Product 3',
+  description: "The third and final sample product",
+  price: 16
+}]
