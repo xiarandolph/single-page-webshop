7f6a8344c17ea941dd4b7f2a843c6cec99aabc37
Randolph Xia <xia.randolph.227@gmail.com>
2018-06-16 12:23:46 -0400

    Change table to articles
    Done for better modularity

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,21 @@
 Vue.component('shop', {
   template: '#shop-template',
   props: {
-    data: Array,
-    keys: Array,
+    data: Array
   },
   data: function() {
-    var sortOrders = {}
-    this.keys.forEach(function(key) {
-      sortOrders[key] = 1 //stores sort order for each column, 1 or -1 for ascending or descending
-    })
     return {
-      sortKey: '',
-      sortOrders: sortOrders
+      selection: null
     }
   },
   computed: {
     filteredData: function() {
-      var sortKey = this.sortKey
-      var order = this.sortOrders[sortKey] || 1
-      var data = this.data
-
-      if (sortKey) { //sorts data based on column sortKey and order
-        data = data.slice().sort(function(a, b) {
-          a = a[sortKey]
-          b = b[sortKey]
-          return (a === b ? 0 : a > b ? 1 : -1) * order
-        })
-      }
-      return data
-    }
-  },
-  filters: {
-    capitalize: function(str) {
-      return str.charAt(0).toUpperCase() + str.slice(1)
+      return (this.selection ? this.selection.options : this.data)
     }
   },
   methods: {
-    sortBy: function(key) {
-      this.sortKey = key
-      this.sortOrders[key] = this.sortOrders[key] * -1
+    select: function(selection) {
+      this.selection = selection
     }
   }
 })
@@ -47,7 +24,6 @@ var app = new Vue({
   el: '#app',
   data: {
     motd: "Welcome to the shop",
-    shopKeys: ['name', 'description', 'price'],
     shopData: productList //defined in products.js
   }
 })
--- src/js/products.js
+++ src/js/products.js
@@ -1,13 +1,26 @@
 var productList = [{
-  name: 'Product 1',
+  name: "Product 1",
   description: 'The first of many',
-  price: 5
+  options: [{
+    name: "Type A",
+    price: "5"
+  }, {
+    name: "Type B",
+    price: "18",
+    options: [{
+      name: "Type BA",
+      price: "19"
+    }, {
+      name: "Type BB",
+      price: "27"
+    }]
+  }]
 }, {
-  name: 'Product 2',
+  name: "Product 2",
   description: "The second one",
   price: 7
 }, {
-  name: 'Product 3',
+  name: "Product 3",
   description: "The third and final sample product",
   price: 16
 }]
