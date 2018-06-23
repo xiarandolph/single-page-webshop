943012aa08af07037a2feaab1688e217e76f137a
Randolph Xia <xia.randolph.227@gmail.com>
2018-06-22 20:55:12 -0400

    Include fourth sample product

--- assets/products.js
+++ assets/products.js
@@ -1,17 +1,2 @@
-let products = [{
-  "id": "P1",
-  "price": 17,
-  "name": "Product 1",
-  "description": "First one"
-}, {
-  "id": "P2",
-  "price": 9,
-  "name": "Product 2",
-  "description": "The second one"
-}, {
-  "id": "P3",
-  "price": 12,
-  "name": "Product 3",
-  "description": "the third and final one"
-}]
+let products = [{"id": "P1", "price": 17, "name": "Product 1", "description": "First one"}, {"id": "P2", "price": 9, "name": "Product 2", "description": "The second one"}, {"id": "P3", "price": 12, "name": "Product 3", "description": "The third and final one"}, {"id": "P4", "price": 23, "name": "Product 4", "description": "Testing css overflow"}]
 export default products
