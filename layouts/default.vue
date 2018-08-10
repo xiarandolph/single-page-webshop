44309d4a92234a1f8105da36b2d0498ba838d034
Randolph Xia <xia.randolph.227@gmail.com>
2018-08-10 16:01:41 -0400

    Set up project skeleton
    Remove starter template clutter
    
    Change generated insperity.vue to cart.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,9 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
-      :mini-variant.sync="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
+      clipped
       fixed
+      v-model="drawer"
       app
     >
       <v-list>
@@ -24,57 +23,26 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
+    
+    <v-toolbar fixed app clipped-left clipped-right>
       <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-toolbar-title>Single Page Webshop</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         icon
-        @click.stop="rightDrawer = !rightDrawer"
+        to="/cart"
       >
-        <v-icon>menu</v-icon>
+        <v-icon>shopping_cart</v-icon>
       </v-btn>
     </v-toolbar>
+    
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
+    
+    <v-footer fixed app>
     </v-footer>
   </v-app>
 </template>
@@ -83,17 +51,10 @@
   export default {
     data () {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
+        drawer: false,
         items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+          { icon: 'home', title: 'Home', to: '/' }
+        ]
       }
     }
   }
---
+++ pages/cart.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      Also nothing here yet.
+    </v-flex>
+  </v-layout>
+</template>
